5c9b2de0111c509da44b3fb9e836c67b338e3564
Oliver Leontiev <leontiev.oliver@gmail.com>
2024-04-06 09:31:14 +0200

    Kostra servera

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-    "log"
-    "os"
-    "strings"
-    "github.com/gin-gonic/gin"
-    "github.com/revilO602/ambulance-webapi/api"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/revilO602/ambulance-webapi/api"
+	"github.com/revilO602/ambulance-webapi/internal/ambulance_wl"
 )
 
 func main() {
-    log.Printf("Server started")
-    port := os.Getenv("AMBULANCE_API_PORT")
-    if port == "" {
-        port = "8080"
-    }
-    environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
-    if !strings.EqualFold(environment, "production") { // case insensitive comparison
-        gin.SetMode(gin.DebugMode)
-    }
-    engine := gin.New()
-    engine.Use(gin.Recovery())
-    // request routings
-    engine.GET("/openapi", api.HandleOpenApi)
-    engine.Run(":" + port)
+	log.Printf("Server started")
+	port := os.Getenv("AMBULANCE_API_PORT")
+	if port == "" {
+		port = "8080"
+	}
+	environment := os.Getenv("AMBULANCE_API_ENVIRONMENT")
+	if !strings.EqualFold(environment, "production") { // case insensitive comparison
+		gin.SetMode(gin.DebugMode)
+	}
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+	ambulance_wl.AddRoutes(engine)
+	engine.GET("/openapi", api.HandleOpenApi)
+	engine.Run(":" + port)
 }
